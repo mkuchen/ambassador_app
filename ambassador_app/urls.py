f90bf1bc1c32a5831e2a73a2514619c947587c5f
Mitch <mkuchen@umich.edu>
2013-09-29 17:19:33 -0400

    first view, here goes something

--- ambassador_app/urls.py
+++ ambassador_app/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.auth.decorators import login_required, permission_required
+from referral_center.views import ReferralCreate
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -8,10 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ambassador_app.views.home', name='home'),
     # url(r'^ambassador_app/', include('ambassador_app.foo.urls')),
-
+    url(r'^$', login_required(ReferralCreate.as_view(template_name="home.html"))),
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
