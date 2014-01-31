9cc7b227c37a0b2afcf7366f6fae1f93749bcb13
Mitch <mkuchen@umich.edu>
2014-01-31 05:54:21 -0500

    finished styles to profile page form, image uploading now works and looks pretty

--- referral_center/models.py
+++ referral_center/models.py
@@ -1,21 +1,30 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from cloudinary.models import CloudinaryField
+
 import urllib
+
+from south.modelsinspector import add_introspection_rules
+add_introspection_rules([], ["^cloudinary\.models\.CloudinaryField"])
 
 class Member(models.Model):
 	user = models.OneToOneField(User)
 	display_name = models.CharField(max_length=60, default="Referral Marketing Solutions")
 	quote = models.CharField(max_length=300, default="Let's get things rolling!")
 	bio = models.TextField(default="")
-	#image = models.TextField(blank=True, null=True)
-	image = models.FileField(upload_to='documents/%Y/%m/%d', blank=True, null=True)
+	profile_image = CloudinaryField("image", default=None)
 
 class Referral(models.Model):
 	link_title = models.CharField(max_length=500)
 	date_submitted = models.DateTimeField(auto_now_add=True)
-	logo_url = models.CharField(max_length=1000)
-	banner_background_url = models.CharField(max_length=1000)
+	#logo_url = models.CharField(max_length=1000)
+	#banner_background_url = models.CharField(max_length=1000)
+	logo_image = CloudinaryField("image", default=None)
+	banner_image = CloudinaryField("image", default=None)
 	banner_text = models.CharField(max_length=1000)
 	font_family = models.CharField(max_length=500)
 
